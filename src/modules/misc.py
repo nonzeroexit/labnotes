b507275fbd980f68bba6303f4a6a0c4a74c67c84
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-12 10:43:45 -0300

    calling function from the same module

--- src/modules/misc.py
+++ src/modules/misc.py
@@ -1,18 +1,17 @@
 import sys
 import os
-from modules import misc
 
 def get_projects_path():
     projects_path = os.getenv('projects_path')
     if projects_path is None:
-        misc.error('projects_path env var not configured')
+        error('projects_path env var not configured')
     return projects_path
 
 def get_projects():
     projects_path = get_projects_path()
     projects = sorted([folder for folder in os.listdir(projects_path) if os.path.isdir(os.path.join(projects_path, folder))])
     if len(projects) == 0:
-        misc.error('No projects found', 0)
+        error('No projects found', 0)
     return projects
 
 def error(msg, code = 1):
