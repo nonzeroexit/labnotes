b54dab67a38a7de8964ce8aeada4a36fcb3119c5
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-13 13:03:16 -0300

    refactor get_n_notes()

--- src/modules/usr_input.py
+++ src/modules/usr_input.py
@@ -30,13 +30,8 @@ def get_search_query():
     return query
 
 def get_n_notes():
-    n_notes = -1 # all notes
-    try:
-        n_notes = int(sys.argv[2])
-        if n_notes < 1:
-            misc.error('last_n_notes must be bigger than 0')
-    except ValueError:
-        misc.error(f'{sys.argv[2]} is not a valid number')
-    except IndexError:
-        pass
-    return n_notes
+    if len(sys.argv) < 3:
+        return -1 # all notes
+    if sys.argv[2].isdigit() and int(sys.argv[2]) > 0:
+        return int(sys.argv[2])
+    misc.error(f'{sys.argv[2]} is not a valid number')
