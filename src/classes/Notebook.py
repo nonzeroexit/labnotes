cb1df01cc9cd77f84cc89202a3883403b1d24d47
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-17 10:00:55 -0300

    changed method name

--- src/classes/Notebook.py
+++ src/classes/Notebook.py
@@ -10,7 +10,7 @@ class Notebook:
         self.path = os.path.join(get_projects_path(), project, 'labnotes.txt')
         self.has_content = os.path.isfile(self.path) and os.path.getsize(self.path) > 0
 
-    def get_content(self):
+    def _get_content(self):
         with open(self.path, 'r', encoding='utf-8') as fhandle:
             notebook_content = fhandle.read().strip()
             return notebook_content
@@ -18,7 +18,7 @@ class Notebook:
     def get_notes(self, n_notes: int):
         date = NO_DATE
         notes = []
-        for i, line in enumerate(self.get_content().split('\n')):
+        for i, line in enumerate(self._get_content().split('\n')):
             if line.startswith('#'):
                 date = line.strip(' #')
             if line.startswith('*'):
@@ -28,7 +28,7 @@ class Notebook:
     def add_note(self, note: str):
         date = get_date.today().strftime("%d-%m-%y")
         with open(self.path, 'a', encoding='utf-8') as fhandle:
-            if date not in self.get_content(): # first note of the day
+            if date not in self._get_content(): # first note of the day
                 fhandle.write(f'### {date}\n')
             fhandle.write(f'* {note}\n')
         print(f'Note added successfully to {self.project}')
@@ -36,7 +36,7 @@ class Notebook:
     def search_note(self, query: str):
         hits = []
         date = NO_DATE
-        for line in self.get_content().split('\n'):
+        for line in self._get_content().split('\n'):
             if line.startswith('###'):
                 date = line.strip(' #')
             if query in line:
